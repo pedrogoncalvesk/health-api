29d072ca4e787671fc175101129ebd45bb4cac23
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2023-11-24 20:41:22 -0300

    chore: update README.md

--- src/modules/service/entities/service.entity.ts
+++ src/modules/service/entities/service.entity.ts
@@ -14,37 +14,7 @@ export class ServiceEntity {
   @PrimaryGeneratedColumn({
     name: 'ID',
     type: 'raw',
-    // select: false,
-    // transformer: {
-    //   from: (value: Buffer) => {
-    //     console.log('from', value);
-    //     return value.toString('hex');
-    //   },
-    //   to: (value: string) => {
-    //     console.log('to', value, Buffer.from(value, 'hex'));
-    //     // return Buffer.from(value, 'hex');
-    //     return value;
-    //   },
-    // },
   } as any)
-  // @PrimaryColumn({
-  //   name: 'ID',
-  //   type: 'raw',
-  //   length: 16,
-  //   // generatedType: 'VIRTUAL',
-  //   // select: false,
-  //   // default: null,
-  // transformer: {
-  //   from: (value: Buffer) => {
-  //     console.log('from', value);
-  //     return value.toString('hex');
-  //   },
-  //   to: (value: string) => {
-  //     console.log('to', value, Buffer.from(value, 'hex'));
-  //     return Buffer.from(value, 'hex');
-  //   },
-  // },
-  // })
   id: string;
 
   @Column({ name: 'NAME', type: 'varchar2', length: 255, nullable: false })
